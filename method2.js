04b1c75fabcb5e58ca63cc6f5c2dfbc91760ea66
hcy <956885844@qq.com>
2017-05-07 18:55:55 +0800

    方法三

--- method2.js
+++ method2.js
@@ -7,10 +7,11 @@ Array.prototype.arrayToHeavy2 = function () {
 
     for(var  i = 1;i < this.length;){
         this[i] === result[result.length - 1] ? i++ : result.push(this[i])
+
     }
 
 return result;
 };
 
 var arr = [1,1,23,22,33,33,"33","a","1","a","b","b","23","33"];
-console.log(arr.arrayToHeavy2())
+console.log(arr.arrayToHeavy2());
--- method3.js
+++ method3.js
@@ -1,3 +1,18 @@
 /**
  * Created by hcy on 2017/5/7.
  */
+Array.prototype.arrayToHeavy3  =function () {
+    var result = [];
+    var json = {};
+    for(var i =0; i < this.length; i++){
+        if(!json[this[i]]){
+            result.push(this[i]);
+            json[this[i]] = 1;
+
+        }
+    }
+    return result;
+};
+
+var arr = [1,1,23,22,33,33,"33","a","1","a","b","b","23","33"];
+console.log(arr.arrayToHeavy3());
